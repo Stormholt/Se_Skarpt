e314e84f45d0db29bd0892ce004b8c07c72c68c6
Ajs <arist@adm.dtu.dk>
2020-11-20 14:21:00 +0100

    Slettet bmp280driver, skabt kopi af wpf så jeg kan lege med det

---
+++ WpfApp1 - AjsCopy/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SQLite;
+//using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        sqLitedatabase databaseObject = new sqLitedatabase();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Filldata();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            databaseObject.OpenConnection();
+            databaseObject.sendData(1, 2);
+            databaseObject.CloseConnection();
+            Filldata();
+        }
+
+        private void Filldata()
+        {
+            Dataset.ItemsSource = databaseObject.Filldata().DefaultView;
+        }
+    }
+}
