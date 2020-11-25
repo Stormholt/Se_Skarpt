3a45f29b294a0495459d497d50a124cdc44fbc8c
Jeppe Møller Bak <jmba@dtu.dk>
2020-11-25 11:37:08 +0100

    Temp/light gauge

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
+using System.ComponentModel;
+
 
 namespace WpfApp1
 {
@@ -28,17 +30,22 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         sqLitedatabase databaseObject = new sqLitedatabase();
+        private double _value; // Angular Gauge
 
-        
+
         public MainWindow()
         {
             InitializeComponent();
             Filldata();
+
+            Value = 160; //Angular Gauge
+            // Temp chat
             ChartValues<double> tempList = new ChartValues<double>();
+            ChartValues<double> datetimeList = new ChartValues<double>();
             for(int i = 0; i < databaseObject.Filldata().Rows.Count; i++)
             {
                 tempList.Add(Convert.ToDouble(databaseObject.Filldata().Rows[i]["temp"]));
-                Labels = new[] {Convert.ToString(databaseObject.Filldata().Rows[i]["temp"]) };
+                //datetimeList.Add(Convert.ToDouble(databaseObject.Filldata().Rows[i]["datetime"]));
             }
             SeriesCollection = new SeriesCollection
             {
@@ -46,12 +53,14 @@ namespace WpfApp1
                 {
                     Title = "Temp",
                     Values = tempList
+                   // Labels = datetimeList
                 },
             };
 
             
             Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            DataContext = this;
+            // temp chart end 
+            DataContext = this; // livechart 
             
         }
 
@@ -67,6 +76,35 @@ namespace WpfApp1
         {
             Dataset.ItemsSource = databaseObject.Filldata().DefaultView;
         }
+
+
+
+        //Angular Gauge
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        private void ChangeValueOnClick(object sender, RoutedEventArgs e)
+        {
+            Value = new Random().Next(50, 250);
+        }
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //Angular Gauge end 
+
+        // temp chart 
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
     }
