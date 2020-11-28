9e4a6634ae548e37251a6da4de71c842bdf4e55a
Ajs <arist@adm.dtu.dk>
2020-11-28 11:17:40 +0100

    clean up is esp app begyndt

---
+++ espnfproject-uden-ssl/ESPnanoFrameworkApp.cs
@@ -0,0 +1,138 @@
+﻿//Nanoframework specific classes
+using System;
+using System.Threading;
+using System.Diagnostics;
+using Windows.Devices.WiFi;
+using Windows.Devices.Adc;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Windows.Devices.Gpio;
+
+//Classes created for this project
+using espSensors;
+using SeSkarptEnumClasses;
+using RGBled;
+
+
+
+namespace ESPnanoFrameworkApp
+{
+    
+
+    public class ESPclient
+    {
+
+        // Set the SSID & Password to your local WiFi network
+        // Ajs mobil hotspot
+        /*const string MYSSID = "AndroidAP7914";
+        const string MYPASSWORD = "lqbk3422";
+        const string IP_ADDR = "192.168.43.61";*/
+
+        // Ajs hjemme wifi
+        /* const string MYSSID = "WiFimodem-BAB4";
+         const string MYPASSWORD = "gjz4gjzntz";
+         const string IP_ADDR = "192.168.0.13";*/
+
+        //Jeppe hotspot
+        const string MYSSID = "ajsertilguys";
+        const string MYPASSWORD = "ajsajsbby";
+        const string IP_ADDR = "192.168.43.15";
+
+        const int TCP_PORT = 8001;
+
+        const int photoresistor_PIN = 0;
+        const int LM35_PIN = 3;
+        const int LEDR_PIN = 2;
+        const int LEDG_PIN = 0;
+        const int LEDB_PIN = 4;
+
+        WiFiAdapter wifi;
+        IPEndPoint ep;
+        Socket tcpclnt;
+
+        Command command = Command.None;
+        Ledcolor ledcolor = Ledcolor.Off;
+        RGBLED rgbled;
+
+        AdcController adcController;
+        Photoresistor photoresistor;
+        LM35 lm35;
+
+        public ESPclient()
+        {
+            rgbled = new RGBLED(GpioController.GetDefault().OpenPin(LEDR_PIN),
+                                GpioController.GetDefault().OpenPin(LEDG_PIN),
+                                GpioController.GetDefault().OpenPin(LEDB_PIN));
+            adcController = AdcController.GetDefault();
+            adcController.ChannelMode = AdcChannelMode.SingleEnded;    // ADC value comes from a single point 
+            photoresistor = new Photoresistor(adcController.OpenChannel(photoresistor_PIN));// ADC channel 0 - Photoresistor
+        }
+
+
+
+
+        private void ConnectToWifi()
+        {
+            wifi = WiFiAdapter.FindAllAdapters()[0];
+            wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
+
+            Debug.WriteLine("starting WiFi scan");
+            wifi.ScanAsync();
+            Thread.Sleep(10000);
+
+            Socket tcpclnt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Debug.WriteLine("Created tcp client socket");
+
+        }
+
+        private static void Wifi_AvailableNetworksChanged(WiFiAdapter sender, object e)
+        {
+            Debug.WriteLine("Wifi_AvailableNetworksChanged - get report");
+
+            // Get Report of all scanned WiFi networks
+            WiFiNetworkReport report = sender.NetworkReport;
+
+            // Enumerate though networks looking for our network
+            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
+            {
+                // Show all networks found
+                //  Debug.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
+
+                // If its our Network then try to connect
+                if (net.Ssid == MYSSID)
+                {
+                    // Disconnect in case we are already connected
+                    sender.Disconnect();
+
+                    // Connect to network
+                    WiFiConnectionResult result = sender.Connect(net, WiFiReconnectionKind.Automatic, MYPASSWORD);
+
+                    // Display status
+                    if (result.ConnectionStatus == WiFiConnectionStatus.Success)
+                    {
+
+                        Debug.WriteLine("Connected to Wifi network");
+                        break;
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"Error {result.ConnectionStatus.ToString()} connecting o Wifi network");
+                    }
+                }
+            }
+
+
+        }
+
+        private void ConnectToServer()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP_ADDR), TCP_PORT);
+            Debug.WriteLine("Created endpoint to server");
+            Debug.WriteLine("Connecting.....");
+            tcpclnt.Connect(ep);
+            Debug.WriteLine("Connected to server");
+
+        }
+    }
+}
--- espnfproject-uden-ssl/Program.cs
+++ espnfproject-uden-ssl/Program.cs
@@ -13,59 +13,25 @@ using Windows.Devices.Gpio;
 using espSensors;
 using SeSkarptEnumClasses;
 using RGBled;
+using ESPnanoFrameworkApp;
 
 namespace espnfproject
 {
 
     public class Program
     {
-        // Set the SSID & Password to your local WiFi network
-        // Ajs mobil hotspot
-        /*const string MYSSID = "AndroidAP7914";
-        const string MYPASSWORD = "lqbk3422";
-        const string IP_ADDR = "192.168.43.61";*/
-
-        // Ajs hjemme wifi
-        /* const string MYSSID = "WiFimodem-BAB4";
-         const string MYPASSWORD = "gjz4gjzntz";
-         const string IP_ADDR = "192.168.0.13";*/
-
-        //Jeppe hotspot
-        const string MYSSID = "ajsertilguys";
-        const string MYPASSWORD = "ajsajsbby";
-        const string IP_ADDR = "192.168.43.15";
-
-        const int TCP_PORT = 8001;
-
-        const int photoresistor_PIN = 0;
-        const int LM35_PIN = 3;
-        const int LEDR_PIN = 2;
-        const int LEDG_PIN = 0;
-        const int LEDB_PIN = 4;
+       
         
         public static void Main()
         {
             try
             {
-                WiFiAdapter wifi = WiFiAdapter.FindAllAdapters()[0];
-                wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
-
-                Debug.WriteLine("starting WiFi scan");
-                wifi.ScanAsync();
-                Thread.Sleep(10000);
                 
-                Socket tcpclnt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                Debug.WriteLine("Created tcp client socket");
                 
-                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP_ADDR), TCP_PORT);
-                Debug.WriteLine("Created endpoint to server");
-                Debug.WriteLine("Connecting.....");
-                tcpclnt.Connect(ep);
-                Debug.WriteLine("Connected to server");
+               
 
                
-                Command command = Command.None;
-                Ledcolor ledcolor = Ledcolor.Off;
+                
 
                 RGBLED rgbled = new RGBLED(GpioController.GetDefault().OpenPin(LEDR_PIN),
                                              GpioController.GetDefault().OpenPin(LEDG_PIN),
@@ -145,44 +111,7 @@ namespace espnfproject
         /// <param name="sender"></param>
         /// <param name="e"></param>
 
-        private static void Wifi_AvailableNetworksChanged(WiFiAdapter sender, object e)
-        {
-            Debug.WriteLine("Wifi_AvailableNetworksChanged - get report");
-
-            // Get Report of all scanned WiFi networks
-            WiFiNetworkReport report = sender.NetworkReport;
-
-            // Enumerate though networks looking for our network
-            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
-            {
-                // Show all networks found
-                //  Debug.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
-
-                // If its our Network then try to connect
-                if (net.Ssid == MYSSID)
-                {
-                    // Disconnect in case we are already connected
-                    sender.Disconnect();
-
-                    // Connect to network
-                    WiFiConnectionResult result = sender.Connect(net, WiFiReconnectionKind.Automatic, MYPASSWORD);
-
-                    // Display status
-                    if (result.ConnectionStatus == WiFiConnectionStatus.Success)
-                    {
-                        
-                        Debug.WriteLine("Connected to Wifi network");
-                        break;
-                    }
-                    else
-                    {
-                        Debug.WriteLine($"Error {result.ConnectionStatus.ToString()} connecting o Wifi network");
-                    }
-                }
-            }
-     
-           
-        }
+      
     }
 
 
