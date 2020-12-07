88b35e6386d7b2ba6f92bc1edd5e44564d1ba717
Ajs <arist@adm.dtu.dk>
2020-12-07 13:46:26 +0100

    Final push

--- espnfproject/Program.cs
+++ espnfproject/Program.cs
@@ -1,376 +1,60 @@
 using System;
+//Nanoframework specific classes
 using System.Threading;
 using System.Diagnostics;
-using Windows.Devices.WiFi;
-using System.Net;
-using System.Net.Sockets;
-using System.Net.Security;
-using System.Text;
-using nanoFramework.Hardware.Esp32;
-using Windows.Devices.Gpio;
-using System.Collections;
 
+//Classes created for this project
+using SeSkarptEnumClasses;
+using ESPnanoFrameworkApp;
 
-namespace esptest0
+namespace espnfproject
 {
 
     public class Program
     {
-        // Set the SSID & Password to your local WiFi network
-        /*const string MYSSID = "AndroidAP7914";
-        const string MYPASSWORD = "lqbk3422";
-        const string IP_ADDR = "192.168.43.61";*/
-
-        const string MYSSID = "WiFimodem-BAB4";
-        const string MYPASSWORD = "gjz4gjzntz";
-        const string IP_ADDR = "192.168.0.13";
-        const int TCP_PORT = 8001;
-        // static Command commands = new Command();
-        //   [Flags]
-        //   enum Command {GetData, SetData, Disconnect, None, SayHello }
-        static Ledcolor ledcolor = Ledcolor.Off;
-        static GpioPin _redLED = GpioController.GetDefault().OpenPin(2);
-        static GpioPin _greenLED = GpioController.GetDefault().OpenPin(0);
-        static GpioPin _blueLED = GpioController.GetDefault().OpenPin(4);
 
         public static void Main()
         {
             try
             {
-                // Get the first WiFI Adapter
-                WiFiAdapter wifi = WiFiAdapter.FindAllAdapters()[0];
-                // Set up the AvailableNetworksChanged event to pick up when scan has completed
-                wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
+                ESPclient client = new ESPclient(); 
+                client.ConnectToWifi();
+                client.ConnectToServer();
 
-                Console.WriteLine("starting WiFi scan");
-                wifi.ScanAsync();
-                Thread.Sleep(10000);
-                
-                Socket tcpclnt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                Console.WriteLine("Created tcp client socket");
-                // need an IPEndPoint from that one above
-                
-                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP_ADDR), TCP_PORT);
-                Console.WriteLine("Created endpoint to server");
-                //   TcpClient tcpclnt = new TcpClient();
-                Console.WriteLine("Connecting.....");
-                tcpclnt.Connect(ep);
-                Console.WriteLine("Connected to server");
-
-                
-
-                using (SslStream stream = new SslStream(tcpclnt))
+                while (true)
                 {
-                    Console.WriteLine("Authenticating server");
-                    
-                    stream.SslVerification = SslVerification.NoVerification;
+                    client.command = Command.None;
 
-                    // stream.AuthenticateAsClient(IP_ADDR, SslProtocols.Tls12);
-                    stream.AuthenticateAsClient(null, SslProtocols.None);
+                    client.ReadCommand();
 
-                    Command command = Command.None;
-                    Ledcolor ledcolor = Ledcolor.Off;
-
-                    //Initiate GPIO controller
-                    //var gpioController = GpioController.GetDefault();
-                    // Open connections for LED pins
-
-                    // Define GPIO pin mode
-                    _redLED.SetDriveMode(GpioPinDriveMode.Output);
-                    _greenLED.SetDriveMode(GpioPinDriveMode.Output);
-                    _blueLED.SetDriveMode(GpioPinDriveMode.Output);
-                    Configuration.SetPinFunction(25, DeviceFunction.ADC1_CH8);
-
-                    while (true)
+                    switch (client.command.Value)
                     {
-                        // setup buffer to read data from socket
-                        command = Command.None;
-                        byte[] buffer = new byte[1024];
-
-                        // trying to read from socket
-                        // int bytes = tcpclnt.Receive(buffer);
-                        // int bytes = stream.Read(buffer, 0, buffer.Length);
-                        //  Debug.WriteLine($"Read {bytes} bytes");
-                        string message = ReadMessage(stream);
-                        if (Command.Disconnect.Name.Equals(message))
-                        {
-                            command = Command.Disconnect;
-                        }
-                        else if (Command.None.Name.Equals(message))
-                        {
-                            command = Command.None;
-                        }
-                        else if (Command.GetData.Name.Equals(message))
-                        {
-                            command = Command.GetData;
-                        }
-                        else if (Command.SetData.Name.Equals(message))
-                        {
-                            command = Command.SetData;
-                        }
-                        else if (Command.SayHello.Name.Equals(message))
-                        {
-                            command = Command.SayHello;
-                        }
-                        /*  if (bytes > 0)
-                          {
-                              // we have data!
-                              // output as string
-                              Debug.WriteLine(new String(Encoding.UTF8.GetChars(buffer)));
-
-                              if (Command.Disconnect.Name.Equals(new String(Encoding.UTF8.GetChars(buffer))))
-                              {
-                                  command = Command.Disconnect;
-                              }
-                              else if (Command.None.Name.Equals(new String(Encoding.UTF8.GetChars(buffer))))
-                              {
-                                  command = Command.None;
-                              }
-                              else if (Command.GetData.Name.Equals(new String(Encoding.UTF8.GetChars(buffer))))
-                              {
-                                  command = Command.GetData;
-                              }
-                              else if (Command.SetData.Name.Equals(new String(Encoding.UTF8.GetChars(buffer))))
-                              {
-                                  command = Command.SetData;
-                              }
-                              else if (Command.SayHello.Name.Equals(new String(Encoding.UTF8.GetChars(buffer))))
-                              {
-                                  command = Command.SayHello;
-                              }
-
-                          }*/
-
-                        switch (command.Value)
-                        {
-                            case 0://DISCONNNECT
-                                tcpclnt.Close();
-                                Thread.Sleep(Timeout.Infinite);
-                                break;
-                            case 1://SAYHELLOface
-                                Console.Write("Transmitting hello world : ");
-                                buffer = Encoding.UTF8.GetBytes("Hello world !\r\n");
-
-                                //tcpclnt.Send(buffer);
-                                stream.Write(buffer,0, buffer.Length);
-                                stream.Flush();
-                                Debug.WriteLine($"Send {buffer.Length} bytes");
-                                break;
-                            case 2://GETDATA
-
-                                buffer = Encoding.UTF8.GetBytes(Gpio.IO25.ToString());
-
-                                //tcpclnt.Send(buffer);
-                                stream.Write(buffer, 0, buffer.Length);
-                                stream.Flush();
-
-                                Debug.WriteLine($"Send {buffer.Length} bytes");
-                                break;
-                            case 3://SETDATA
-                                buffer = new byte[1024];
-                                message = ReadMessage(stream);
-                                Debug.WriteLine(message);
-                                controlLED(message);
-                                // trying to read from socket
-                                /*  bytes = tcpclnt.Receive(buffer);
-
-                                  if (bytes > 0)
-                                  {
-                                      Debug.WriteLine(new String(Encoding.UTF8.GetChars(buffer)));
-                                      controlLED(new String(Encoding.UTF8.GetChars(buffer)));
-                                  }*/
-                                break;
-                            case 4: //NONE
-                                break;
-                        }
+                        case 0://DISCONNNECT
+                            client.rgbled.controlLED(Ledcolor.Off); // turn off LED
+                            client.tcpclnt.Close(); //Close TCP Connection.
+                            Thread.Sleep(Timeout.Infinite);
+                            break;
+                        case 1://SAYHELLO
+                            client.SendHello();
+                            break;
+                        case 2://GETDATA
+                            client.SendData();
+                            break;
+                        case 3://SETDATA
+                            client.ReadRGBcolor();
+                            break;
+                        case 4: //NONE
+                            break;
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("message:" + ex.Message);
-                Console.WriteLine("stack:" + ex.StackTrace);
+                Debug.WriteLine("message:" + ex.Message);
+                Debug.WriteLine("stack:" + ex.StackTrace);
             }
 
-            Thread.Sleep(Timeout.Infinite);
+            
         }
-        static string ReadMessage(SslStream sslStream)
-        {
-            // Read the  message sent by the server.
-            // The end of the message is signaled using the
-            // "<EOF>" marker.
-            byte[] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-          /*  do
-            {*/
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
-
-                // Use Decoder class to convert from bytes to UTF8
-                // in case a character spans two buffers.
-                Decoder decoder = Encoding.UTF8.GetDecoder();
-                // char[] chars = new char[decoder. GetCharCount(buffer, 0, bytes)];
-                // decoder.GetChars(buffer, 0, bytes, chars, 0);
-                char[] chars = new char[1024];
-                bool completed;
-                int byteUsed;
-                int charsUsed;
-                decoder.Convert(buffer, 0, buffer.Length, chars, 0, chars.Length, true, out byteUsed, out charsUsed, out completed);
-                messageData.Append(chars);
-                // Check for EOF.
-             /*   if (messageData.ToString().IndexOf("<EOF>") != -1)
-                {
-                    break;
-                }
-            } while (bytes != 0);*/
-
-            return messageData.ToString();
-        }
-
-        private static void controlLED(string command)
-        {
-            if (Ledcolor.Red.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Red;
-                _greenLED.Write(GpioPinValue.Low);
-                _blueLED.Write(GpioPinValue.Low);
-                _redLED.Write(GpioPinValue.High);
-                Console.WriteLine(ledcolor.Name);
-            }
-            else if (Ledcolor.Green.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Green;
-                _redLED.Write(GpioPinValue.Low);
-                _blueLED.Write(GpioPinValue.Low);
-                _greenLED.Write(GpioPinValue.High);
-                Console.WriteLine(ledcolor.Name);
-            }
-            else if (Ledcolor.Blue.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Blue;
-                _redLED.Write(GpioPinValue.Low);
-                _greenLED.Write(GpioPinValue.Low);
-                _blueLED.Write(GpioPinValue.High);
-                Console.WriteLine(ledcolor.Name);
-            }
-            else if (Ledcolor.Magenta.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Magenta;
-                _greenLED.Write(GpioPinValue.Low);
-                _redLED.Write(GpioPinValue.High);
-                _blueLED.Write(GpioPinValue.High);
-
-                Console.WriteLine(ledcolor.Name);
-            }
-            else if (Ledcolor.Cyan.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Cyan;
-                _greenLED.Write(GpioPinValue.High);
-                _redLED.Write(GpioPinValue.Low);
-                _blueLED.Write(GpioPinValue.High);
-                Console.WriteLine(ledcolor.Name);
-            }
-            else if (Ledcolor.Yellow.Name.Equals(command))
-            {
-                ledcolor = Ledcolor.Yellow;
-                _blueLED.Write(GpioPinValue.Low);
-                _greenLED.Write(GpioPinValue.High);
-                _redLED.Write(GpioPinValue.High);
-                Console.WriteLine(ledcolor.Name);
-            }
-            else
-            {
-                ledcolor = Ledcolor.Off;
-                _redLED.Write(GpioPinValue.Low);
-                _greenLED.Write(GpioPinValue.Low);
-                _blueLED.Write(GpioPinValue.Low);
-                Console.WriteLine(ledcolor.Name);
-            }
-
-        }
-
-        /// <summary>
-        /// Event handler for when WiFi scan completes
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-
-        private static void Wifi_AvailableNetworksChanged(WiFiAdapter sender, object e)
-        {
-            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
-
-            // Get Report of all scanned WiFi networks
-            WiFiNetworkReport report = sender.NetworkReport;
-
-            // Enumerate though networks looking for our network
-            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
-            {
-                // Show all networks found
-                //  Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
-
-                // If its our Network then try to connect
-                if (net.Ssid == MYSSID)
-                {
-                    // Disconnect in case we are already connected
-                    sender.Disconnect();
-
-                    // Connect to network
-                    WiFiConnectionResult result = sender.Connect(net, WiFiReconnectionKind.Automatic, MYPASSWORD);
-
-                    // Display status
-                    if (result.ConnectionStatus == WiFiConnectionStatus.Success)
-                    {
-                        
-                        Console.WriteLine("Connected to Wifi network");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error {result.ConnectionStatus.ToString()} connecting o Wifi network");
-                    }
-                }
-            }
-
-           
-        }
-    }
-    public class Command
-    {
-        public static Command Disconnect { get; } = new Command(0, "Disconnect");
-        public static Command SayHello { get; } = new Command(1, "SayHello");
-        public static Command GetData { get; } = new Command(2, "GetData");
-        public static Command SetData { get; } = new Command(3, "SetData");
-        public static Command None { get; } = new Command(4, "None");
-
-        public string Name { get; private set; }
-        public int Value { get; private set; }
-
-        private Command(int val, string name)
-        {
-            Value = val;
-            Name = name;
-        }
-
-    }
-
-    public class Ledcolor
-    {
-        public static Ledcolor Red { get; } = new Ledcolor(0, "Red");
-        public static Ledcolor Blue { get; } = new Ledcolor(1, "Blue");
-        public static Ledcolor Green { get; } = new Ledcolor(2, "Green");
-        public static Ledcolor Magenta { get; } = new Ledcolor(3, "Magenta");
-        public static Ledcolor Cyan { get; } = new Ledcolor(4, "Cyan");
-        public static Ledcolor Yellow { get; } = new Ledcolor(5, "Yellow");
-        public static Ledcolor White { get; } = new Ledcolor(6, "White");
-        public static Ledcolor Off { get; } = new Ledcolor(7, "Off");
-        public string Name { get; private set; }
-        public int Value { get; private set; }
-
-        private Ledcolor(int val, string name)
-        {
-            Value = val;
-            Name = name;
-        }
-
     }
 }
---
+++ espnfproject/SeSkarptEnumClasses.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SeSkarptEnumClasses
+{
+    public class Command // An implementation of Enum class.
+    {
+        public static Command Disconnect { get; } = new Command(0, "Disconnect");
+        public static Command SayHello { get; } = new Command(1, "SayHello");
+        public static Command GetData { get; } = new Command(2, "GetData");
+        public static Command SetData { get; } = new Command(3, "SetData");
+        public static Command None { get; } = new Command(4, "None");
+
+        public string Name { get; private set; }
+        public int Value { get; private set; }
+
+        private Command(int val, string name)
+        {
+            Value = val;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Method to find and return Command object based on a string with same name.
+        /// </summary>
+        /// <param name="contender"></param>
+        /// <returns Command></returns>
+
+        public Command FindCommand(string contender) 
+        {
+            if (Command.Disconnect.Name.Equals(contender))
+            {
+                return Command.Disconnect;
+            }
+            else if (Command.GetData.Name.Equals(contender))
+            {
+                return Command.GetData;
+            }
+            else if (Command.SetData.Name.Equals(contender))
+            {
+                return Command.SetData;
+            }
+            else if (Command.SayHello.Name.Equals(contender))
+            {
+                return Command.SayHello;
+            }
+            else if (Command.None.Name.Equals(contender))
+            {
+                return Command.None;
+            }
+            else
+            {
+                Debug.WriteLine("Invalid command, no matches! returned 'None' command");
+                return Command.None;
+            }
+
+        }
+
+    }
+    public class Ledcolor // Enum implementation
+    {
+        public static Ledcolor Red { get; } = new Ledcolor(0, "Red");
+        public static Ledcolor Blue { get; } = new Ledcolor(1, "Blue");
+        public static Ledcolor Green { get; } = new Ledcolor(2, "Green");
+        public static Ledcolor Magenta { get; } = new Ledcolor(3, "Magenta");
+        public static Ledcolor Cyan { get; } = new Ledcolor(4, "Cyan");
+        public static Ledcolor Yellow { get; } = new Ledcolor(5, "Yellow");
+        public static Ledcolor White { get; } = new Ledcolor(6, "White");
+        public static Ledcolor Off { get; } = new Ledcolor(7, "Off");
+        public string Name { get; private set; }
+        public int Value { get; private set; }
+
+        private Ledcolor(int val, string name)
+        {
+            Value = val;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Method to find and return Ledcolor object based on the name
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns Ledcolor ></returns>
+        public Ledcolor FindLedcolor(string command)
+        {
+            if (Ledcolor.Red.Name.Equals(command))
+            {
+                return Ledcolor.Red;
+            }
+            else if (Ledcolor.Green.Name.Equals(command))
+            {
+                return Ledcolor.Green;
+            }
+            else if (Ledcolor.Blue.Name.Equals(command))
+            {
+                return Ledcolor.Blue;       
+            }
+            else if (Ledcolor.Magenta.Name.Equals(command))
+            {
+                return Ledcolor.Magenta;
+            }
+            else if (Ledcolor.Cyan.Name.Equals(command))
+            {
+                return Ledcolor.Cyan;
+            }
+            else if (Ledcolor.Yellow.Name.Equals(command))
+            {
+                return Ledcolor.Yellow;
+            }
+            else if (Ledcolor.White.Name.Equals(command))
+            {
+                return Ledcolor.White;
+            }
+            else if (Ledcolor.Off.Name.Equals(command))
+            {
+                return Ledcolor.Off;
+            }
+            else
+            {
+                Debug.WriteLine("Invalid color, no matches! returned 'Off' Ledcolor");
+                return Ledcolor.Off;
+            }
+
+        }
+
+    }
+}
